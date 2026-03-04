e963b30140e833747bba2e9bc2e91de7b74d10c1
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-03-03 19:06:15 -0800

    add : dependencies added.

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
+  resolve: {
+    alias: [{ find: /^~(.*)$/, replacement: '$1' }],
+  },
+  server: {
+    open: true,
+  },
+});
